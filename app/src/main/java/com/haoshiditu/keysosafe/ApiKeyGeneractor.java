1ea2a3010bf57d87cf6d4dd8d8ce2c0980680e76
luoruiyi <luoruiyi2008@163.com>
2017-06-22 18:00:50 +0800

    更改文件结构，直接导入便可以运行，更加方便。同时增加源码说明

---
+++ app/src/main/java/com/haoshiditu/keysosafe/ApiKeyGeneractor.java
@@ -0,0 +1,12 @@
+package com.haoshiditu.keysosafe;
+
+import android.content.Context;
+
+/**
+ * @author luoruiyi
+ */
+
+public class ApiKeyGeneractor {
+    public static native String keyFromJNI(Context context);
+
+}
---
+++ app/src/main/java/com/haoshiditu/keysosafe/MainActivity.java
@@ -0,0 +1,25 @@
+package com.haoshiditu.keysosafe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    // 虚拟机在加载类时候加载 so
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        String txt = ApiKeyGeneractor.keyFromJNI(this);
+        tv.setText(txt);
+    }
+}
